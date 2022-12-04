1bb3c14911d8dba3e3e1b61b6e1def759472afe8
Gabriel Andre <89227472+Gabrielandre02@users.noreply.github.com>
2022-12-03 23:12:39 -0300

    Project one

---
+++ DAY ONE/project_one.py
@@ -0,0 +1,14 @@
+# welcome
+
+print("welcome to the band name generator")
+# Name city
+
+city = input("Name of the city?\n")
+
+# Name Pet 
+
+pet = input("Name of the pet?\n")
+
+band = city+pet
+
+print(band)
